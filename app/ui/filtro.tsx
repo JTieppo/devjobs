54d61978963f9ffc9c345288e9a70c1a956a7440
Tieppo <88698431+JTieppo@users.noreply.github.com>
2024-02-02 19:27:42 -0300

    .

--- app/ui/filtro.tsx
+++ app/ui/filtro.tsx
@@ -3,10 +3,6 @@ import { useState, useEffect } from "react";
 
 
 export default function Filtro(content) {
-    const [checkVisual, setCheckVisual] = useState('');
-    function mostraCheck(){
-        setCheckVisual(checkVisual == '' ? 'md:inline-block':'');
-    }
     return (
         <div className="m-10 absolute mx-auto w-full top-56">    
             <div className="flex flex-col mx-auto mr-10 ml-10">
@@ -17,11 +13,11 @@ export default function Filtro(content) {
                 <div>
                     <div id="input-text" className="flex flex-col justify-between p-6 rounded-b-lg">
                         <div className="flex flex-row">
-                            <input onClick={mostraCheck} type="checkbox" id="meuCheckbox" className="" />
-                            <label htmlFor="meuCheckbox" className="label-checkbox">Full</label>
+                            <input type="checkbox" id="checkbox-mobile" className="flex my-auto mr-2" />
+                            <label htmlFor="meuCheckbox" className="">Full</label>
                             <p className="ml-1">time</p>
                             <p className="ml-1">only</p>
-                            <img src="/assets/desktop/icon-check.svg" className={`hidden ${checkVisual} relative md:ml-[-91px] lg:ml-[-127px] pointer-events-none`} alt="" />
+                            <img src="/assets/desktop/icon-check.svg" className="relative md:ml-[-91px] lg:ml-[-127px] pointer-events-none" alt="" />
                         </div>
                         <button className="mt-2 rounded bg-[#5964E0] h-10 w-full">search</button>
                     </div>
